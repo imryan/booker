9e63f22b3ad63468b3504e202bcc496fd91a4583
Ryan Cohen <notryancohen@gmail.com>
2013-07-31 14:53:43 -0400

    Filtering by date now works!

--- Filter/Filter.cs
+++ Filter/Filter.cs
@@ -22,22 +22,27 @@ namespace Booker
             string date = datePicker.Value.ToShortDateString();
             int count = Program.booker.listView.Items.Count;
 
-            /*
             ListViewItem searchItem = null;
             int index = 0;
             do
             {
-                index = index == 0 ? 0 : Math.Min(Program.booker.listView.Items.Count - 1, index);
-                searchItem = Program.booker.listView.FindItemWithText(date, true, index);
-                if (searchItem != null)
+                if (index < Program.booker.listView.Items.Count)
                 {
-                    index = searchItem.Index + 1;
+                    //true = search subitems
+                    //last false param = no partial matches (remove if you want partial matches)
+                    searchItem = Program.booker.listView.FindItemWithText(date, true, index, false);
+                    if (searchItem != null)
+                    {
+                        index = searchItem.Index + 1;
 
-                    //rest of code
+                        Program.booker.listView.MultiSelect = true;
+                        searchItem.Selected = true;
+                    }
                 }
+                else
+                    searchItem = null;
 
             } while (searchItem != null);
-             */
         }
     }
 }
