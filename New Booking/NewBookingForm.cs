b183fe0ed125fab737f00709cb1d20003b9eb706
Ryan Cohen <notryancohen@gmail.com>
2013-08-14 10:56:22 -0400

    Features added
    Checks if reservation already exists before it create a new one.

--- New Booking/NewBookingForm.cs
+++ New Booking/NewBookingForm.cs
@@ -13,6 +13,8 @@ namespace Booker
 {
     public partial class newBookingForm : Form
     {
+        public bool canAdd;
+
         public newBookingForm()
         {
             InitializeComponent();
@@ -22,6 +24,26 @@ namespace Booker
             roomField.Items.Add("Sales Demo Unit 1");
             roomField.Items.Add("Sales Demo Unit 2");
             roomField.Items.Add("Other");
+        }
+
+        private void checkIfExists(string[] array)
+        {
+            int count = Program.booker.listView.Items.Count;
+            bool hasShown = false;
+            canAdd = true;
+
+            for (int i = 0; i < count; i++)
+            {
+                ListViewItem currentItem = Program.booker.listView.Items[i];
+                if (currentItem.SubItems[1].ToString().Contains(array[1]) &&
+                    currentItem.SubItems[0].ToString().Contains(array[0]) &&
+                    currentItem.SubItems[2].ToString().Contains(array[2]) && !hasShown)
+                {
+                    MessageBox.Show("There is already a reservation for your selection.\nFor:\t" + array[0] + ".\nOn:\t" + array[1] + "." + "\nFrom:\t" + array[2] + ".", "Alert");
+                    hasShown = true;
+                    canAdd = false;
+                }
+            }
         }
 
         private async void newBookingButton_Click(object sender, EventArgs e)
@@ -46,16 +68,22 @@ namespace Booker
                 string person = ParseUser.CurrentUser.Username;
 
                 string[] array = new string[4] { room, date, time, person };
-                Program.booker.UpdatingListView(array);
 
-                ParseObject booking = new ParseObject("Booking");
-                booking["room"] = room;
-                booking["date"] = date;
-                booking["time"] = time;
-                booking["person"] = person;
-                await booking.SaveAsync();
+                checkIfExists(array);
 
-                Dispose();
+                if (canAdd)
+                {
+                    Program.booker.UpdatingListView(array);
+
+                    ParseObject booking = new ParseObject("Booking");
+                    booking["room"] = room;
+                    booking["date"] = date;
+                    booking["time"] = time;
+                    booking["person"] = person;
+                    await booking.SaveAsync();
+
+                    Dispose();
+                }
             }
         }
     }
